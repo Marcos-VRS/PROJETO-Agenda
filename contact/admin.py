9d87e43b62b175283884da6fe9948a1b9a39b32a
Marcos Vinícius <marcos31051991@gmail.com>
2024-08-12 17:52:11 -0300

    Models criados e página do admin organizada

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from contact.models import Contact
+
 
 # Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "phone",
+        "email",
+        "created_date",
+    )
+
+    ordering = ("-id",)
+    search_fields = (
+        "id",
+        "first_name",
+        "last_name",
+    )
+    list_per_page = 1
+    list_max_show_all = 200
+    list_editable = (
+        "first_name",
+        "last_name",
+    )
+    list_display_links = (
+        "id",
+        "phone",
+    )
